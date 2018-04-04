f154072229c29ce8987b151fc61d0a311bd623d1
smac89 <smac89@users.noreply.github.com>
2018-04-04 11:26:15 -0600

    Added aliases dotfile

---
+++ dotfiles/aliases.sh
@@ -0,0 +1,6 @@
+alias egrep='egrep --color=auto'
+{%@@ if profile == 'home' @@%}
+alias jython='TERM=xterm-color jython'
+{%@@ elif profile == 'skip' @@%}
+alias skip-search='egrep -RiIn --exclude-dir="target/" --exclude-dir=".idea/"'
+{%@@ endif @@%}
